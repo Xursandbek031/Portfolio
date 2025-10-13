650076871c0a6c8321e98e988976410568093ce0
Xursandbek031 <xursandbek99tatuuf@gmail.com>
2025-10-13 15:50:50 +0500

    navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
+
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+const navLinks = document.querySelectorAll('.nav_link');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu');
+    navToggle.innerHTML = navMenu.classList.contains('show-menu')
+        ? '<i class="fa-solid fa-xmark"></i>'
+        : '<i class="fa-solid fa-bars"></i>';
+});
+
+// menyu band bosilganda yopish
+navLinks.forEach(link =>
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+        navToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    })
+);
+
+
+
+
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
+const scrollActive = () => {
     const scrollDown = window.scrollY
 
-  sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('nav a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('nav a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link')
-        }else{
+        } else {
             sectionsClass.classList.remove('active-link')
-        }                                                    
+        }
     })
 }
 window.addEventListener('scroll', scrollActive)
@@ -26,7 +49,7 @@ const sr = ScrollReveal({
     delay: 200,
 });
 
-sr.reveal('.home-data, .about-img',{}); 
-sr.reveal('.home-img, .about_subtitle, .about_text, .skills-img',{delay: 400}); 
-sr.reveal('.home_social-icon',{ interval: 200}); 
-sr.reveal('.skill, .contact_input, .submit-btn',{interval: 200}); 
+sr.reveal('.home-data, .about-img', {});
+sr.reveal('.home-img, .about_subtitle, .about_text, .skills-img', { delay: 400 });
+sr.reveal('.home_social-icon', { interval: 200 });
+sr.reveal('.skill, .contact_input, .submit-btn', { interval: 200 }); 
